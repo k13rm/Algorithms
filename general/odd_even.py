37625da673c118989d7d9489bb47b99dbba77b5c
Nazir Nayal <nnayal17@ku.edu.tr>
2025-03-29 10:16:00 +0100

    add odd even problem

---
+++ general/odd_even.py
@@ -0,0 +1,33 @@
+# x = 2
+
+# x = 3 + 2
+# y = 3 - 2
+# z = 3 * 2
+# w = 3 / 2
+# # modulo operatore / remainder operator
+# x = int(input("Enter a number, I'll say if it's odd or even:"))
+
+# if x % 2 == 0:
+#     print("the number is even")
+# if x % 2 != 0:
+#     print("the number is odd")
+lst = [3, 5, 1, 4, 8, 2, 9, 11, 23, 22]
+# even_count = 0
+# odd_count = 0
+# for x in lst:
+#     if x % 2 == 1:
+#         odd_count += 1
+#     else:
+#         even_count += 1
+# print("There are", odd_count, "odd numbers,")
+# print("and lastly there are", even_count, "even numbers.")    
+
+
+# 
+        
+"""
+Homework:
+
+Given the list above, find the largest odd number in the list and lowest even number in the list.
+the print the difference between
+"""
--- general/practice.py
+++ general/practice.py
@@ -5,41 +5,82 @@ Homework:
 1. count how many duplicates there are in the list
 2. find all duplicates and how many times each number is duplicated
 """
-count = 0
 
-#1:
-for i in range(len(lst)):
-    for j in range(i + 1, len(lst)):
-        if lst[i] == lst[j]:
-            print("yes")
-#2:
-duplicates = 0
-for i in range(len(lst)):
-    for j in range(i + 1, len(lst)):
-        if lst[i] == lst[j]:
-            duplicates += 1
-print(duplicates)
+c = {}
+for x in lst:
+    if x in c:
+        c[x] = c[x]+1
+    else:
+        c[x] = 1
+
+for key, value in c.items():
+    print(key, "is duplicated", value, "times.")
+
+
+
+
+# count = 0
+
+# #1:
+# for i in range(len(lst)):
+#     for j in range(i + 1, len(lst)):
+#         if lst[i] == lst[j]:
+#             print("yes")
+# #2:
+# duplicates = 0
+# for i in range(len(lst)):
+#     for j in range(i + 1, len(lst)):
+#         if lst[i] == lst[j]:
+#             duplicates += 1
+# print(duplicates)
 
 
 # (key, value) pairs
 # 
+# d = {}
 
-mydictionary = {
-    "Mert": "The elegant Universe",
-}
+# mydictionary = {
+#     "Mert": "The elegant Universe",
+# }
 
-mydictionary["Ekin"] = "The essence of Calculus"
-mydictionary["Volkan"] = ["The art programming", "Leaders of Tomorrow"]
-mydictionary["Ekin"] = "The story of the three little pigs"
+# mydictionary["Ekin"] = "The essence of Calculus"
+# mydictionary["Volkan"] = ["The art programming", "Leaders of Tomorrow"]
+# mydictionary["Ekin"] = "The story of the three little pigs"
 
-print("Mert's favorite book is", mydictionary["Mert"])
+# # del mydictionary["Ekin"]
 
-if "Selim" in mydictionary:
-    print("Selim's favorite book is", mydictionary["Selim"])
-else:
-    print("Selim is not in the dictionary")
+# print("Mert's favorite book is", mydictionary["Mert"])
 
+# if "Selim" in mydictionary:
+#     print("Selim's favorite book is", mydictionary["Selim"])
+# else:
+#     print("Selim is not in the dictionary")
+"""
 
+counter = {}
+
+l = [3, 6, 1, 7, 2, 1]
+
+for x in l:
+    if x in counter:
+        counter[x] = counter[x] + 1
+    else:
+        counter[x] = 1
+
+print(counter)
+
+# 1: go over the keys
+for key in counter.keys():
+    print(key, "is duplicated", counter[key], "times")
+
+# 2: go over the values
+for value in counter.values():
+    print(value)
+
+# 3: go over the key-value pairs
+for key, value in counter.items():
+    print(key, "is duplicated", value, "times")
+"""
 #if storage[1] == storage[len]
 
 # lst2 = lst[2:6] # [7, 17, 28, 18]

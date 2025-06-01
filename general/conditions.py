ab90761fd6f8d07d3319a0c2dcb39580370123da
Nazir Nayal <nnayal17@ku.edu.tr>
2025-06-01 16:32:17 +0200

    add new problems

--- general/conditions.py
+++ general/conditions.py
@@ -54,13 +54,13 @@ comparison operators:
 """
 
 
-x = 1
-y = 2
+# x = 1
+# y = 2
 
-z = (x > y) # False
-z = False
+# z = (x > y) # False
+# z = False
 
-z = (x > y)
+# z = (x > y)
 
 # if z: # sth has to be a boolean, whether a boolean variable or a boolean statement
 
@@ -71,17 +71,17 @@ You can combine boolean statements using the following operators:
 3. not: reverses the boolean statement
 """
 
-if (x > y):
-    if (y > z):
-        print("x is greater than y") 
+# if (x > y):
+#     if (y > z):
+#         print("x is greater than y") 
 
-if (x > y) and (y > z):
-    print("x is greater than y and y is greater than z")
+# if (x > y) and (y > z):
+#     print("x is greater than y and y is greater than z")
 
-if (x > y) or (y > z):
+# if (x > y) or (y > z):
 
-if not (x > y):
-    print("x is not greater than y")
+# if not (x > y):
+#     print("x is not greater than y")
 
 
 """
@@ -99,15 +99,65 @@ F: 0 - 59
 
 
 
-print("Put your grade in for your class, the grade scale will determine wether you've gotten an A or B and etc.")
-grade = int(input("Enter grade here (ex: 90 or 76):"))
-if grade >= 89:
-    print("You got an 'A'")
-if grade <= 90 and >= 79:
-    print("You got a 'B'")
-if grade <= 80  and >= 69:
-    print("You got a 'C'")
-if grade <= 70 and >= 59:
-    print("You got a 'D'")
-if grade <= 60 and >= 0:
-    print("You got a 'F'")
+# print("Put your grade in for your class, the grade scale will determine wether you've gotten an A or B and etc.")
+# grade = int(input("Enter grade here (ex: 90 or 76):"))
+# if grade >= 90:
+#     print("You got an 'A'")
+# elif grade >= 80:
+#     print("You got a 'B'")
+# elif grade >= 70:
+#     print("You got a 'C'")
+# elif grade >= 60:
+#     print("You got a 'D'")
+# else:
+#     print("You got a 'F'")
+
+
+# """
+# Given a list of numbers, if the list has the number 2 or 5, increase the counter by one
+# for each number. If the list has the numbers 3 and 7, decrease the counter by one.
+# """
+# counter = 0
+# lst = [1, 2, 3, 2, 4, 5, 7, 8 ,9]
+# if 2 in lst:
+#     counter += 1
+# if 5 in lst:
+#     counter += 1
+# if 3 in lst:
+#     counter -= 1
+# if 7 in lst:
+#     counter -= 1
+
+
+"""
+Problem: We have two friends, Jonathan and Rick. They want to go on a school trip. However, in order for their teacher to allow them to go to the trip, they must score high at the maths exam.
+This means the sum of both of their scores needs to be over 70. If both of them score over 90, they will get free ice cream on the trip. You will take the scores of Jonathan and Rick as input,
+and then first you need to tell them if they will go on the trip or not, and then you need to tell them if they will get ice cream or not.
+
+"""
+
+# Jscore = 0
+# Rscore = 0
+
+# Jscore = int(input("How much did Jonathan score? \ntype here:"))
+# Rscore = int(input("How much did Rick score? \ntype here:"))
+
+# if (Jscore + Rscore) / 2 > 69:
+#   print("They will go on a trip.")
+# else:
+#   print("They both failed the exam so they won't be able to go on a trip.")
+# if (Jscore + Rscore) / 2 > 89:
+#   print("Also they will get free ice cream.")
+
+
+"""
+Homework Problem: We have two friends, Jonathan and Rick. They want to go on a school trip. However, in order for their teacher to allow them to go to the trip, they must score high at the maths exam.
+This means the sum of both of their scores needs to be over 70. If both of 
+them score over 90, they will get free ice cream on the trip. In addition, if either of them scores below 60, they can to the trip but whoever scored below 60 must pay a fine.
+
+For example, if Jonathan scores 55 and Rick scores 90, they will both still go, but Jonathan will have to pay a fine because he scored less than 60
+
+ You will take the scores of Jonathan and Rick as input,
+and then first you need to tell them if they will go on the trip or not, and then you need to tell them if they will get ice cream or not. If someone will pay fine you should print a message specifying that.`
+
+"""

a721a826ebdfb7a74e95ab5eecfa50c48933fbf1
Nazir Nayal <nnayal17@ku.edu.tr>
2025-08-26 21:01:33 +0200

    complete is_winner function

--- general/winner_of_tictactoe.py
+++ general/winner_of_tictactoe.py
@@ -8,13 +8,13 @@ board = [['-' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 #  ['-', '-', '-', '-', '-']
 #  ['-', '-', '-', '-', '-']]
 
-board[4][0] = "X"
-board[4][1] = "X"
+board[6][0] = "X"
+board[5][1] = "X"
 board[4][2] = "X"
-board[4][3] = "X"
-board[4][4] = "X"
-board[4][5] = "X"
-board[4][6] = "X"
+board[3][3] = "X"
+board[2][4] = "X"
+board[1][5] = "X"
+board[0][6] = "O"
 
 
 # [(0,0), (0,1), (0,2), (0,3), (0,4)],
@@ -25,6 +25,9 @@ board[4][6] = "X"
 # Goal (2): given the current player as input, return True if all the numbers in at least one of rows is equal to the current player, otherwise, return False.
 
 # Goal (3): given the current player as input, return True if all the numbers in at least one of the rows or one of the columns is equal to the current player, otherwise, return False.
+
+# Goal (4): create a new function iswinner_short and make it such that it can do the exact same thing as the iswinner2 function, but with a maximum of 2 for loops.
+
 
 def is_winner(current_player):
 
@@ -54,25 +57,52 @@ def is_winner2(current_player):
         if flag == True: 
             # this means that row j has all the elements equal to current_player
             return True
-    
-    return False
-
-def is_winner2(current_player):
-    
+            
+   
     for k in range (BOARD_SIZE): # number of columns
         
-        
         flag = True
-        for l in range(BOARD_SIZE): # number of columns
-            if board[k][l] != current_player:
+        for l in range(BOARD_SIZE): # number of rows
+            if board[l][k] != current_player:
                 flag = False
                 break
         if flag == True: 
             
             return True
     
+    flag = True
+    for k in range(BOARD_SIZE):
+
+        if board[k][k] != current_player:
+            flag = False
+            break
+        
+    if flag == True:
+        return True
+    
+    flag = True    
+    for r in range(BOARD_SIZE):
+
+        col = r
+        row = BOARD_SIZE - 1 - r
+
+        if board[BOARD_SIZE - 1 - r][r] != current_player:
+            flag = False
+            break
+
+    if flag == True:
+        return True
+    
+    
     return False
 
+
+# (BOARD_SIZE - 1 - 0, 0) ( , r)
+# (BOARD_SIZE - 1 - 1, 1)
+# (BOARD_SIZE - 1 - 2, 2)
+# (BOARD_SIZE - 1 - 3, 3)
+# ....
+# (0, BOARD_SIZE - 1)
 
     
 

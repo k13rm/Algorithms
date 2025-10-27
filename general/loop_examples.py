1f6c6278be60babeb2b99a21b395724f8ee4a386
Kerem DURUK <keremduruk12@gmail.com>
2025-10-27 19:39:29 +0300

    homework done

--- general/loop_examples.py
+++ general/loop_examples.py
@@ -17,11 +17,20 @@
 # 10
 
 
+#type1
 n = 10
 for i in range(1, n):
     print(i)
 
 
+#type 2
+for k in range(5, 25):
+    print(i)
+
+#type 3
+for h in range(4, 5, 14):
+    print(h)
 # Type 1: for i in range(n): [0, 1, 2, 3, ..., n-1], increases by 1 every time
 # Type 2: for i in range(a,b): the for loop starts at a, ends at b-1, increases by 1 every time. So the total number or rounds is (b - a)
 # Type 3: for i in range(a,b,c):
+

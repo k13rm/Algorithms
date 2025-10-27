104e27b08cfcbbeeb86df58e6e541ef1e61b09ee
Nazir Nayal <nnayal17@ku.edu.tr>
2025-10-27 19:00:16 +0100

    add while loop

--- general/loop_examples.py
+++ general/loop_examples.py
@@ -18,19 +18,104 @@
 
 
 #type1
-n = 10
-for i in range(1, n):
-    print(i)
+# n = 10
+# for i in range(n):
+#     print(i)
 
 
 #type 2
-for k in range(5, 25):
-    print(i)
+# for k in range(5, 25): # [5, 6, 7, ..., 24]
+#     print(i)
+
+# for k in range(20): [0, 1, 2, ..., 19]
 
 #type 3
-for h in range(4, 5, 14):
-    print(h)
+# for h in range(2, 100, 2): # [2, 4, 6, 8, 10, 12, ..., 98]
+#     print(h)
 # Type 1: for i in range(n): [0, 1, 2, 3, ..., n-1], increases by 1 every time
 # Type 2: for i in range(a,b): the for loop starts at a, ends at b-1, increases by 1 every time. So the total number or rounds is (b - a)
 # Type 3: for i in range(a,b,c):
 
+# Exercise: print all the odd numbers between 33 and 200
+
+
+# for k in range(33, 200, 2):
+#     print(k)
+
+#### While Loop
+# booleans are variables that can be either True or False, and are often used on if-statements
+
+# a = 10
+# b = 20
+
+# while a < b:
+#     print("Hello World")
+#     # do something
+#     b = b - 2
+
+# an Infinite Loop
+
+# n = 10
+# for i in range(n):
+#     print(i)
+
+# i = 0
+# while i < n:
+#     print(i)
+#     i = i + 1
+
+# i = 0
+
+# for i in range(20):
+#     print(i)
+
+
+# while i < 20:
+#     print(i)
+#     i = i + 1
+
+# i = 5
+
+# for i in range(5, 20):
+#     print(i)
+
+# while i < 20:
+#     print(i)
+#     i = i + 1
+
+
+# for i in range(10, 50, 5):
+#     print(i)
+
+# i = 10
+
+# while i < 50:
+#     print(i)
+#     i = i + 5
+
+# Exercise: Use a for loop or a while loop to print the numbers from 20 to 5 backwards
+
+# k = 20
+
+# while k > 5:
+#     print(k)
+#     k = k-1
+
+# for k in range(20, 5, -1):
+#     print(k)
+
+# 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49: Arithmetic Series/Sequence/Progression
+# base = 4, initial = 1
+
+# Arithmetic series, that initial = 3, base of 7, print all numbers in this series up to 61
+
+for k in range(3, 61, 7):
+    print(k)
+
+# complete the following series up to a 100
+# 1, 2, 4, 7, 11, 16, 22, 29, 37, ....
+k = 0
+h = k + 1
+while k < 100:
+    print(k)
+    k = h + 1

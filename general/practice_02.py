dc7bffe393fe57c8d0e7bb4d50d3e2ca45b95c63
Nazir Nayal <nnayal17@ku.edu.tr>
2026-04-12 13:56:48 +0200

    add new practice file

---
+++ general/practice_02.py
@@ -0,0 +1,31 @@
+# Problem 1: Given a list of numbers, find the maximum and minimum values in the list.
+# lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+# maximum_value = -999999
+# minimum_value= 999999
+# for k in range(0, len(lst)):
+#     if lst[k] > maximum_value:
+#         maximum_value = lst[k]
+#     if lst[k] < minimum_value:
+#         minimum_value = lst[k]
+
+# print("the maximum value is:", maximum_value)
+# print("the minimum value is:", minimum_value)
+
+# maximum_value = max(lst)
+# minimum_value = min(lst)
+
+
+# Problem 2: Given a list of numbers, find the sum of the odd numbers in the list.
+# lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+# sumofodd = 0
+# for k in range(0, len(lst)):
+#     if lst[k] % 2 == 1:
+#         sumofodd = sumofodd + lst[k]
+
+# print("the sum of all the odd numbers are:", sumofodd)
+
+# sum_of_all = sum(lst)
+# sum_of_even = sum_of_all - sumofodd
+# print("the sum of all the even numbers are:", sum_of_even)
+
+# Problem 3 (slightly harder): Given a list of numbers, find the largest odd number and the smallest even number in the list and print both of them.

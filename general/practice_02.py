63a2d5d677b7561e85fcec5053b6ababb55ad625
Nazir Nayal <nnayal17@ku.edu.tr>
2026-05-03 13:58:57 +0200

    add new problem

--- general/practice_02.py
+++ general/practice_02.py
@@ -29,3 +29,42 @@
 # print("the sum of all the even numbers are:", sum_of_even)
 
 # Problem 3 (slightly harder): Given a list of numbers, find the largest odd number and the smallest even number in the list and print both of them.
+
+"""
+lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+largest_odd_number = -99999
+smallest_even_number = 99999
+for k in range(len(lst)):
+    if lst[k] > largest_odd_number and lst[k] % 2 == 1:
+        largest_odd_number = lst[k]
+    if lst[k] < smallest_even_number and lst[k] % 2 == 0:
+        smallest_even_number = lst[k]
+
+print("The smallest even number in the provided list is:\n", smallest_even_number,"\nThe largest odd number in the provided list is:\n", largest_odd_number)
+"""
+
+# Problem 4: Given a list of numbers, find the number of times each number is duplicated in the list. For example, if the list is [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5], then the output should be:
+# 3 is duplicated 2 times
+# 1 is duplicated 2 times
+# 4 is duplicated 1 times
+# 5 is duplicated 3 times
+# 9 is duplicated 1 times
+# 2 is duplicated 1 times
+# 6 is duplicated 1 times
+
+lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5] # [1, 10000000]
+x = max(lst) + 1
+counter = [0] * x
+
+# [0, 2, 0, 1, 1, 0, 0, 0, 0, 0]
+#  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# dummy_lst = []
+# dummy_lst[5] = dummy_lst[5] + 1
+
+for k in lst:
+    counter[k] = counter[k] + 1
+
+for i in range(x):
+    if counter[i] > 0:
+        print(i, "is duplicated", counter[i], "times")
+    
